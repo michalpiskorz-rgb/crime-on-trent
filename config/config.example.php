06dc18ee1d35ab94456acbde5d82847d2beae2bd
michalpiskorz-rgb <michalpiskorz@gmail.com>
2026-08-26 14:00:14 +0100

    Update config.example.php

--- config/config.example.php
+++ config/config.example.php
@@ -1,1 +1,11 @@
-a
+<?php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'DATABASE_NAME',
+        'user' => 'DATABASE_USER',
+        'password' => 'DATABASE_PASSWORD',
+        'charset' => 'utf8mb4',
+    ],
+];
