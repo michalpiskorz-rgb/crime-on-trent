8d8e2251889f2a12034915b9076da5d732b9c245
michalpiskorz-rgb <michalpiskorz@gmail.com>
2026-08-27 17:51:33 +0100

    Initialize map with OpenStreetMap tile layer

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,1 +1,11 @@
+const map = L.map('map', {
+    zoomControl: true
+}).setView([53.0027, -2.1794], 12);
 
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        maxZoom: 19,
+        attribution: '&copy; OpenStreetMap contributors'
+    }
+).addTo(map);
